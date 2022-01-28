7a9a12f05776ffd486812fa8204e67ba070a5b1c
Bret Hansen <ellucianBret@users.noreply.github.com>
2022-01-28 11:51:17 -0800

    Adding Ellucian Copyright (#2)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Copyright 2021-2022 Ellucian Company L.P. and its affiliates.
+
 export * as experienceUtil from './src/experience.js';
 export * as expressUtil from './src/express.js';
 export * as integrationUtil from './src/integration.js';
